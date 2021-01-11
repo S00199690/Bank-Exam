b63fa14e6721caa79bed4be1b2084edd7d97087c
Ryan Mc Donald <S00199690@mail.itsligo.ie>
2021-01-11 11:24:16 +0000

    selection changed for acccounts

--- OopExam/MainWindow.xaml.cs
+++ OopExam/MainWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace OopExam
             SavingsAccount sa1 = new SavingsAccount("David", "Reavey", 5000.0, "14/05/2020", 100200);
             SavingsAccount sa2 = new SavingsAccount("Lisa", "Maguire", 7500.0, "11/01/2021", 100201);
 
-            //adds each employee to the employees list
+            //adds each account to the accounts list
             accounts.Add(ca1);
             accounts.Add(ca2);
             accounts.Add(sa1);
@@ -49,11 +49,11 @@ namespace OopExam
 
         private void lbxAccounts_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //selected employee object that refers to what is being selected in listbox
-            //the item in the listbox is casted to an employee object
+            //selected account object that refers to what is being selected in listbox
+            //the item in the listbox is casted to an account object
             Account selectedAccount = (Account)lbxAccounts.SelectedItem;
 
-            //checks if the selected item is a part time employee and displays the details in the necessary fields
+            //checks if the selected item is savings account and displays the details in the necessary fields
             if (selectedAccount is SavingsAccount)
             {
                 SavingsAccount account = (SavingsAccount)selectedAccount;
@@ -64,7 +64,7 @@ namespace OopExam
                 tbxTransactionAmt.Text = account.CalculateInterest().ToString();
                
             }
-            //checks if the selected item is a full time employee and displays the details in the necessary fields
+            //checks if the selected item is a current acccount and displays the details in the necessary fields
             else if (selectedAccount is CurrentAccount)
             {
                 CurrentAccount account = (CurrentAccount)selectedAccount;
@@ -78,7 +78,60 @@ namespace OopExam
 
         private void cbxCurrAccounts_Click(object sender, RoutedEventArgs e)
         {
+            //clears any objects currently in the collection and sets the listbox to null
+            filteredAccounts.Clear();
+            lbxAccounts.ItemsSource = null;
 
+            //if both current account and savings account checkboxes are ticked add in all accounts
+            if (cbxCurrAccounts.IsChecked == true && cbxSavAccounts.IsChecked == true)
+            {
+                foreach (Account account in accounts)
+                {
+                    filteredAccounts.Add(account);
+                }
+                //display accounts in listbox
+                lbxAccounts.ItemsSource = filteredAccounts;
+            }
+            else
+            {
+                //if current account is checked and not savings account display current accounts
+                if (cbxCurrAccounts.IsChecked == true && cbxSavAccounts.IsChecked == false)
+                {
+                    foreach (Account account in accounts)
+                    {
+                        if (account is CurrentAccount)
+                        {
+                            filteredAccounts.Add(account);
+                        }
+
+                    }
+                    //display accounts in listbox
+                    lbxAccounts.ItemsSource = filteredAccounts;
+                }
+                //if savings account is checked and not current account display savings accounts
+                else if (cbxSavAccounts.IsChecked == true && cbxCurrAccounts.IsChecked == false)
+                {
+                    foreach (Account account in accounts)
+                    {
+                        if (account is SavingsAccount)
+                        {
+                            filteredAccounts.Add(account);
+                        }
+
+                    }
+                    //display accounts in listbox
+                    lbxAccounts.ItemsSource = filteredAccounts;
+
+                }
+                //if both boxes are not ticked no accounts are displayed
+                else if (cbxSavAccounts.IsChecked == false && cbxCurrAccounts.IsChecked == false)
+                {
+                    //clears the filteredAccounts collection and outputs empty collection
+                    filteredAccounts.Clear();
+                    lbxAccounts.ItemsSource = filteredAccounts;
+                }
+
+            }
         }
     }
 }
